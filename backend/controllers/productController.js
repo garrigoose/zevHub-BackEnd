2819309ae982eb6c6f3538e780b7165566f3af4f
garrigoose <gkats11@gmail.com>
2022-03-22 14:58:41 -0400

    connected to mongodb server, working on multer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,25 @@ const asyncHandler = require('express-async-handler');
 
 const Product = require('../models/productModel');
 const User = require('../models/userModel');
+const multer = require('multer');
 
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.jpg' || ext !== '.png') {
+      return cb(res.status(400).end('only jpg, png are allowed'), false);
+    }
+    cb(null, true);
+  },
+});
+
+var upload = multer({ storage: storage }).single('file');
 // @description     Get products
 // @route/moethod   GET /api/products
 // @access          Private
