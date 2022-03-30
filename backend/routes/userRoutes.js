c8775c54dee48c01e34c62525732eff49f68511a
garrigoose <gkats11@gmail.com>
2022-03-30 16:26:16 -0400

    admin controlled get/update user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,8 @@ const {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
@@ -16,5 +18,7 @@ router.get('/profile', protect, getUserProfile);
 router.get('/', protect, admin, getUsers);
 router.put('/profile', protect, updateUserProfile);
 router.delete('/:id', protect, admin, deleteUser);
+router.get('/:id', protect, admin, getUserById);
+router.put('/:id', protect, admin, updateUser);
 
 module.exports = router;
